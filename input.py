332d06667d3052a95c3ea75bb20d5426571868f2
Fatemeh Gharche <fatemehgharche8@gmail.com>
2025-12-29 09:07:51 +0330

    Add input-reader & Domain

---
+++ input.py
@@ -0,0 +1,33 @@
+# input_reader.py
+from flight import Flight
+
+def read_input():
+    M = int(input())
+
+    MinPrice, MaxPrice = map(int, input().split())
+
+    cities = input().split()
+
+    stays_input = list(map(int, input().split()))
+    stays = []
+    for i in range(0, len(stays_input), 2):
+        stays.append((stays_input[i], stays_input[i+1]))
+
+    flights = []
+    for _ in range(M):
+        o, d, day, price = input().split()
+        flights.append(Flight(o, d, int(day), int(price)))
+
+    return MinPrice, MaxPrice, cities, stays, flights
+
+
+def build_domains(cities, flights):
+    num_vars = len(cities) - 1
+    domains = [[] for _ in range(num_vars)]
+
+    for i in range(num_vars):
+        for f in flights:
+            if f.origin == cities[i] and f.dest == cities[i+1]:
+                domains[i].append(f)
+
+    return domains
